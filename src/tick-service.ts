405ee1b09267e0b42cc02b9c1822080843cb0f35
Jeremy Futral <jfutral@fanatics.com>
2021-03-22 13:48:04 -0400

    reduce code footprint

--- src/tick-service.ts
+++ src/tick-service.ts
@@ -1,9 +1,9 @@
-import { ExchangeName, ExchangePair, PairPriceUpdate } from 'exchange-models/exchange'
+import { ExchangePair, PairPriceUpdate } from 'exchange-models/exchange'
 import { Subscribe } from 'exchange-models/kraken'
 import { LoggerFactoryService } from 'socket-comms-libs'
 import WebSocket from 'ws'
 import * as kraken from 'kraken-helpers'
-import * as c from './common'
+import { Configuration, sleep } from './common'
 
 interface TickerExchangeInterface {
   createTickSubRequest: { (instruments: string[]): Subscribe }
@@ -15,33 +15,32 @@ interface TickerExchangeInterface {
   }
 }
 
-let selectTickerExchange = (exchangeName: ExchangeName): TickerExchangeInterface => {
-  if (exchangeName === 'kraken') {
-    return {
-      createTickSubRequest: kraken.createTickSubRequest,
-      isError: kraken.isError,
-      parseEvent: kraken.parseEvent,
-      getAvailablePairs: kraken.getAvailablePairs,
-      createStopRequest: kraken.createStopRequest,
-    }
-  }
-  console.log("Invalid exchange selected")
-  process.exit(1)
-}
-
 export let tickService = async (
-  conf: c.Configuration,
+  conf: Configuration,
   tickerWS: WebSocket,
-  tickCallback: ((arg: PairPriceUpdate) => void)
-): Promise<Error | undefined> => {
+  tickCallback: (arg: PairPriceUpdate) => void
+): Promise<WebSocket> => {
   // configure ticker behavior
   let isRunning = true
-  const logger = new LoggerFactoryService().getLogger('TickerService')
-  const exi = selectTickerExchange(conf.exchangeName)
+  const logger = new LoggerFactoryService().getLogger('TickService')
+  let exi: TickerExchangeInterface
+  switch (conf.exchangeName) {
+    case 'kraken':
+      exi = {
+        createTickSubRequest: kraken.createTickSubRequest,
+        isError: kraken.isError,
+        parseEvent: kraken.parseEvent,
+        getAvailablePairs: kraken.getAvailablePairs,
+        createStopRequest: kraken.createStopRequest,
+      }
+      break
+    default:
+      throw Error('Invalid exchange selected')
+  }
 
   // attempt to get available pairs
   const pairsResponse = await exi.getAvailablePairs(conf.apiUrl, conf.threshold)
-  if (pairsResponse instanceof Error) return pairsResponse
+  if (pairsResponse instanceof Error) throw pairsResponse
   const pairs = pairsResponse.map(p => p.tradename)
 
   // register handlers
@@ -50,15 +49,14 @@ export let tickService = async (
     if (!isRunning) return
     logger.debug(eventData)
     const event = exi.parseEvent(eventData)
-  
+
     // log then eat any errors
     if (exi.isError(event)) {
-      logger.error(eventData)
       tickerWS.close()
-      process.exit()
+      throw Error(eventData)
     }
 
-    if (typeof event != 'string') tickCallback(event)
+    if (typeof event !== 'string') tickCallback(event)
   })
 
   process.once('SIGINT', async () => {
@@ -66,18 +64,18 @@ export let tickService = async (
     logger.info('Got shutdown request')
     isRunning = false
     tickerWS.send(JSON.stringify(exi.createStopRequest()))
-    await c.sleep(10)
+    await sleep(10)
     tickerWS.close()
     logger.info('Shutdown complete')
   })
 
   // sleep while waiting for client socket state
-  while (tickerWS.readyState === 0) await c.sleep(100)
+  while (tickerWS.readyState === 0) await sleep(100)
   logger.info('Socket ready state: ' + tickerWS.readyState)
 
   // subscribe
   tickerWS.send(JSON.stringify(exi.createTickSubRequest(pairs)))
   logger.info('Subscription request sent')
 
-  return undefined
+  return tickerWS
 }
