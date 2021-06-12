0389755ee6986519e132526bed7ec40314f7da14
Jeremy Futral <jfutral@fanatics.com>
2021-06-12 00:59:06 -0400

    coinbase draft

--- src/exchange/coinbase.ts
+++ src/exchange/coinbase.ts
@@ -1,61 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import tls from 'tls'
 
-// const BeginString = '8=FIX.4.2'
-// const TargetComp = '56=Coinbase'
-// const SenderCompIDBase = '49='
+const BeginString = '8=FIX.4.2'
+const TargetComp = '56=Coinbase'
+const SenderCompIDBase = '49='
 
-// const EncryptMethod = '98=0'
-// const HeartBtInt = '108=30'
-// const PasswordBase = '554='
-// const RawDataBase = '96='
-// const CancelOrdersOnDisconnect = '8013=Y'
-// const DropCopyFlag = '9406=Y'
+const EncryptMethod = '98=0'
+const HeartBtInt = '108=30'
+const PasswordBase = '554='
+const RawDataBase = '96='
+const CancelOrdersOnDisconnect = '8013=Y'
+const DropCopyFlag = '9406=Y'
+let seqnum = 0
 
-// const SendingTime = new Date()
+export const buildHeader = (key: string): [number, string] => [
+  TargetComp.length + SenderCompIDBase.length + key.length,
+  `${BeginString}|${SenderCompIDBase}${key}|${TargetComp}`,
+]
 
-// const buildHeader = (key: string): [number, string] => [
-//   TargetComp.length + SenderCompIDBase.length + key.length,
-//   `${BeginString}|${SenderCompIDBase}${key}|${TargetComp}`,
-// ]
+export const buildLogon = (
+  passphrase: string,
+  signature: string,
+  [headerLength, headerString]: [number, string]
+): [number, string] => [
+  headerLength +
+    EncryptMethod.length +
+    HeartBtInt.length +
+    PasswordBase.length +
+    passphrase.length +
+    RawDataBase.length +
+    signature.length +
+    CancelOrdersOnDisconnect.length +
+    DropCopyFlag.length,
+  `${headerString}|
+  ${EncryptMethod}|
+  ${HeartBtInt}|
+  ${PasswordBase}${passphrase}|
+  ${RawDataBase}${signature}|
+  ${CancelOrdersOnDisconnect}|
+  ${DropCopyFlag}`,
+]
 
-// const buildLogon = (
-//   passphrase: string,
-//   signature: string,
-//   [headerLength, headerString]: [number, string]
-// ): [number, string] => [
-//   headerLength +
-//     EncryptMethod.length +
-//     HeartBtInt.length +
-//     PasswordBase.length +
-//     passphrase.length +
-//     RawDataBase.length +
-//     signature.length +
-//     CancelOrdersOnDisconnect.length +
-//     DropCopyFlag.length,
-//   `${headerString}|
-//   ${HeartBtInt}|
-//   ${PasswordBase}${passphrase}|
-//   ${RawDataBase}${signature}|
-//   ${CancelOrdersOnDisconnect}|
-//   ${DropCopyFlag}`,
-// ]
+export const sign = (key: string, passphrase: string): string =>
+  [new Date(), 'Logon', seqnum, key, 'Coinbase', passphrase].join('\x01')
 
-// const sign = (data: string): string => {
-//   const presign = [
-//     new Date(),
-//     logon.MsgType,
-//     session.outgoing_seq_num,
-//     session.sender_comp_id,
-//     session.target_comp_id,
-//     passphrase
-// ].join('\x01');
-// }
-
-function sleep(ms: number): Promise<unknown> {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
+const sleep = (ms: number): Promise<unknown> => new Promise(resolve => setTimeout(resolve, ms))
 
 export const getConnection = (): tls.TLSSocket => {
+  seqnum++
   const conn = tls.connect(
     {
       host: 'fix-public.sandbox.pro.coinbase.com',
