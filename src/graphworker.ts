91a350f6892170753743219e3807b43629e2bab8
KrispyEnsign <futralj@gmail.com>
2021-06-27 23:02:36 -0400

    Added callback for cleanup

--- src/graphworker.ts
+++ src/graphworker.ts
@@ -8,12 +8,54 @@ import { calcProfit } from './profitcalc.js'
 
 const isError = util.types.isNativeError
 
-export function createGraphProfitCallback(
+const createOrderCallback = (
+  result: Step[],
+  d: GraphWorkerData,
+  t1: number,
+  t3: number,
+  startTime: Date,
+  graphCount: number,
+  ws: unknown,
+  shutdownCallback: () => Promise<void>
+): (() => Promise<void>) => {
+  let seq = 0
+  return async (): Promise<void> => {
+    // shortcircuit and shutdown (for now) if all processed
+    if (++seq >= result.length) {
+      // log and die for now
+      const t2 = Date.now()
+      for (const trade of result) {
+        const pair = d.pairs.find(p => p.tradename === trade.orderCreateRequest.pair)
+        console.log({
+          n: pair?.name,
+          a: pair?.ask,
+          b: pair?.bid,
+          e: trade.price,
+          m: trade.orderCreateRequest.amount,
+          nm: trade.amount,
+          s: trade.orderCreateRequest.direction,
+        })
+      }
+      console.log(`amounts: ${d.initialAmount} -> ${result[result.length - 1].amount}`)
+      console.log(`total latency: ${t2 - t1}ms`)
+      console.log(`mean latency: ${(t2 - t1) / result.length}ms`)
+      console.log(`total calc time: ${t3 - startTime.getTime()}ms`)
+      console.log(`# trades evaluated: ${graphCount}`)
+      await shutdownCallback()
+      return
+    }
+
+    // send the next order
+    sendData(createOrderRequest(d.token, result[seq].orderCreateRequest), ws)
+  }
+}
+
+export const createGraphProfitCallback = (
   d: GraphWorkerData,
   ws: unknown,
   mutex: Mutex,
   shutdownCallback: () => Promise<void>
-): (arg: readonly number[]) => void {
+): ((arg: readonly number[]) => void) => {
   const startTime = new Date(Date.now())
   let graphCount = 0
   return async (cycle: readonly number[]): Promise<unknown> => {
@@ -25,53 +67,29 @@ export function createGraphProfitCallback(
     // check if there was an error
     if (isError(result)) return Promise.reject(result)
 
-    // check if the calc was worthless
-    if (result === 0) return Promise.resolve()
+    // check if the calc was profitable
+    if (result === 0 || result[result.length - 1].amount <= d.initialAmount) return
 
-    // if result is profitable then process
-    if (result[result.length - 1].amount > d.initialAmount) {
-      // lock the processs so no other orders are placed
-      const t3 = Date.now()
-      await mutex.acquire()
+    // lock the processs so no other orders are placed
+    const t3 = Date.now()
+    await mutex.acquire()
 
-      // start sequence
-      let seq = 0
+    // send the initial order
+    sendData(createOrderRequest(d.token, result[0].orderCreateRequest), ws)
+    const orderCallback = createOrderCallback(
+      result,
+      d,
+      t1,
+      t3,
+      startTime,
+      graphCount,
+      ws,
+      shutdownCallback
+    )
 
-      // send the initial order
-      sendData(createOrderRequest(d.token, result[seq].orderCreateRequest), ws)
-
-      // set the callback to place more orders with each response
-      setCallback(ws, async () => {
-        // shortcircuit and shutdown (for now) if all processed
-        if (++seq >= result.length) {
-          // log and die for now
-          const t2 = Date.now()
-          for (const trade of result) {
-            const pair = d.pairs.find(p => p.tradename === trade.orderCreateRequest.pair)
-            console.log({
-              n: pair?.name,
-              a: pair?.ask,
-              b: pair?.bid,
-              e: trade.price,
-              m: trade.orderCreateRequest.amount,
-              nm: trade.amount,
-              s: trade.orderCreateRequest.direction,
-            })
-          }
-          console.log(`amounts: ${d.initialAmount} -> ${result[result.length - 1].amount}`)
-          console.log(`total latency: ${t2 - t1}ms`)
-          console.log(`mean latency: ${(t2 - t1) / result.length}ms`)
-          console.log(`total calc time: ${t3 - startTime.getTime()}ms`)
-          console.log(`# trades evaluated: ${graphCount}`)
-          await shutdownCallback()
-          return
-        }
-
-        // send the next order
-        sendData(createOrderRequest(d.token, result[seq].orderCreateRequest), ws)
-      })
-    }
-    return Promise.resolve()
+    // set the callback to place more orders with each response
+    setCallback(ws, async () => orderCallback)
+    return
   }
 }
 
